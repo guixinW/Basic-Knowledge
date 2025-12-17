0a28fcfc52b3a51330febc99c445eb6c5373ca94
guixinW <1477364283@qq.com>
2025-12-17 17:41:32 +0800

    factory

---
+++ design-patterns/factory/factory_test.go
@@ -0,0 +1,21 @@
+package factory
+
+import "testing"
+
+func compute(factory OperatorFactory, a, b int) int {
+	op := factory.Create()
+	op.SetA(a)
+	op.SetB(b)
+	return op.Result()
+}
+
+func TestOperator(t *testing.T) {
+	var (
+		factory OperatorFactory
+	)
+
+	factory = PlusOperatorFactory{}
+	if compute(factory, 1, 2) != 3 {
+		t.Fatal("error with factory method pattern")
+	}
+}

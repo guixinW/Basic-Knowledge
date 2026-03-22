e317d6308f4f5f8304092f63c2d1912d0468f994
guixinW <1477364283@qq.com>
2026-03-22 17:32:24 +0800

    outline recontruction and add mediator

---
+++ design-patterns/Structual/adapter/adapter_test.go
@@ -0,0 +1,21 @@
+package adapter
+
+import (
+	"testing"
+)
+
+var expect = "adaptee method"
+
+func TestAliyunClientAdapter_CreateServer(t *testing.T) {
+	var a ICreateServer = &AliyunClientAdapter{
+		Client: AliyunClient{},
+	}
+	a.CreateServer(1.0, 2.0)
+}
+
+func TestAwsClientAdapter_CreateServer(t *testing.T) {
+	var a ICreateServer = &AwsClientAdapter{
+		Client: AWSClient{},
+	}
+	a.CreateServer(1.0, 2.0)
+}
